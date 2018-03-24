6afd681967d962b34c308519d06ddac3dadb1378
Ned Letcher <nletcher@gmail.com>
2018-03-24 13:28:01 +1100

    fix up import order

--- dash_skeleton.py
+++ dash_skeleton.py
@@ -5,15 +5,14 @@ creation of simple Plotly Dash apps.
 
 from collections import Counter
 
-from dash import Dash
-from dash.dependencies import Input, State, Output, Event
+import dash
 import dash_core_components as dcc
 import dash_html_components as html
 import plotly.graph_objs as go
-#import dash_table_experiments as dt 
+from dash.dependencies import Input, State, Output, Event
 
 
-app = Dash()
+app = dash.Dash(__name__)
 app.title = 'Dash Skeleton'
 
 # If you need to run your app locally
@@ -50,7 +49,7 @@ chart with their frequency as you type."""),
         ],
         value='no'
     ),
-    dcc.Graph(id='graph')
+    dcc.Graph(id='graph', className='red')
 ])
 
 
@@ -91,46 +90,6 @@ def callback(text, sort_type, normalize):
         },
     }
 
-import os
-def show_callbacks(app):
-    
-    def format_regs(registrations, padding=10):
-        # TODO: -- switch to single line printing if > 79 chars
-        vals = sorted("{}.{}".format(i['id'], i['property'])
-                      for i in registrations)
-        return ", ".join(vals)
-
-    for callback_id, callback in app.callback_map.items():
-        wrapped_func = callback['callback'].__wrapped__
-        inputs = callback['inputs']
-        states = callback['state']
-        events = callback['events']
-        
-        str_values = {
-            'callback': wrapped_func.__name__, 
-            'output': callback_id,
-            'filename': os.path.split(wrapped_func.__code__.co_filename)[-1],
-            'lineno': wrapped_func.__code__.co_firstlineno,
-            'num_inputs': len(inputs),
-            'num_states': len(states),
-            'num_events': len(events),
-            'inputs': format_regs(inputs),
-            'states': format_regs(states),
-            'events': format_regs(events)
-        }
-        
-        output = """
-        callback      {callback} @ {filename}:{lineno}
-        Output        {output}
-        Inputs  {num_inputs:>4}  {inputs}
-        States  {num_states:>4}  {states}
-        Events  {num_events:>4}  {events}
-        """.format(**str_values)
-
-        print(output)
-
-    
-#show_callbacks(app)
 
 if __name__ == '__main__':
     # To make this app publicly available, supply the parameter host='0.0.0.0'.
