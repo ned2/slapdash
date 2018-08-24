6f6bf835dbd1cad004e53a4e6b60ee798a04568a
Ned Letcher <nletcher@gmail.com>
2018-08-24 22:36:04 +1000

    Update Slapdash to use Dash's new system for adding custom asset files

--- src/slapdash/server.py
+++ src/slapdash/server.py
@@ -1,13 +1,14 @@
 import os
 import sys
 
+from dash import Dash
 from flask import Flask, send_from_directory
+from flask.helpers import get_root_path
 
-from .custom_dash import CustomIndexDash
 from .exceptions import HaltCallback
 
+server = Flask(__package__)
 
-server = Flask(__name__)
 
 # load default settings
 server.config.from_object(f'{__package__}.settings')
@@ -18,8 +19,14 @@ server.config.from_object(f'{__package__}.settings')
 server.config.from_envvar('SLAPDASH_SETTINGS', silent=True)
 
 
-app = CustomIndexDash(
+# need to work around a bug in Dash: https://github.com/plotly/dash/issues/345
+# can remove this param from the Dash constructor once fixed
+assets_folder = os.path.join(get_root_path(server.name), 'assets')
+
+
+app = Dash(
     server=server,
+    assets_folder=assets_folder,
     url_base_pathname=server.config['URL_BASE_PATHNAME']
 )
 
