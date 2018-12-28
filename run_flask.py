b052f1750a752735abe4c59934a62ff672ef5bcc
Ned Letcher <nletcher@gmail.com>
2018-12-28 15:58:38 +1100

    Switch to using Dash's run_server() in order to activate dev tools

--- run_flask.py
+++ run_flask.py
@@ -8,24 +8,23 @@ from slapdash.app import app
 def argparser():
     parser = argparse.ArgumentParser()
     parser.add_argument("--port", metavar="PORT", default=8050, type=int)
-    parser.add_argument("--host", metavar="HOST", default='localhost')
-    parser.add_argument('--debug', action='store_true', default=True)
+    parser.add_argument("--host", metavar="HOST", default="localhost")
+    parser.add_argument("--debug", action="store_true", default=False)
     parser.add_argument("--processes", metavar="PROCESSES", type=int, default=1)
-    parser.add_argument("--threaded", action='store_true')
+    parser.add_argument("--threaded", action="store_true")
     return parser
-
 
 
 def main():
     args = argparser().parse_args()
-    app.server.run(
+    app.run_server(
+        port=args.port,
         debug=args.debug,
         host=args.host,
-        port=args.port,
         processes=args.processes,
         threaded=args.threaded,
     )
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
