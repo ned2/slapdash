5822241be35fc7554ee2909a2099feac72e8eb2e
Ned Letcher <nletcher@gmail.com>
2019-01-15 21:10:26 +1100

    Use dash-bootstrap-component Textarea

--- setup.py
+++ setup.py
@@ -6,10 +6,10 @@ setup(
     version='0.1',
     py_modules=['slapdash'],
     install_requires=[
-        'dash>0.26.1',
+        'dash>=0.33.0',
         'dash-html-components',
         'dash-core-components',
-        'dash-bootstrap-components',
+        'dash-bootstrap-components>=0.3.0',
     ],
     packages=find_packages('src'),
     package_dir={'': 'src'},
