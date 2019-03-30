be4a0af9ce09b3f3e178b235350565ee3119e7c5
Ned Letcher <nletcher@gmail.com>
2019-03-30 20:12:59 +1100

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,12 +5,7 @@ setup(
     name="slapdash",
     version="0.1",
     py_modules=["slapdash"],
-    install_requires=[
-        "dash>=0.33.0",
-        "dash-html-components",
-        "dash-core-components",
-        "dash-bootstrap-components>=0.3.0",
-    ],
+    install_requires=["dash>=0.40.0", "dash-bootstrap-components>=0.3.0"],
     packages=find_packages("src"),
     package_dir={"": "src"},
 )
