738b8b2d34fc1b461fd8ba1e3a5e9f1d2da7d80d
Ned Letcher <nletcher@gmail.com>
2019-07-25 12:01:09 +1000

    Fix bug where generated module pathname is incorrect when project name and project slug differ

--- {{cookiecutter.project_slug}}/setup.py
+++ {{cookiecutter.project_slug}}/setup.py
@@ -41,8 +41,9 @@ except FileNotFoundError:
 # dictionary.
 about = {}
 if not VERSION:
-    project_slug = NAME.lower().replace("-", "_").replace(" ", "_")
-    with open(os.path.join(here, "src", project_slug, "__version__.py")) as f:
+    with open(
+        os.path.join(here, "src", "{{ cookiecutter.project_slug }}", "__version__.py")
+    ) as f:
         exec(f.read(), about)
 else:
     about["__version__"] = VERSION
