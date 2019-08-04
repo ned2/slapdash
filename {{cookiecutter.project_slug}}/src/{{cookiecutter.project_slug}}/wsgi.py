7ba7965df6306e96440688ed6dfc53f8d92ed3ce
Ned Letcher <nletcher@gmail.com>
2019-08-05 00:36:37 +1000

    Disable debug mode in wsgi.py

--- {{cookiecutter.project_slug}}/src/{{cookiecutter.project_slug}}/wsgi.py
+++ {{cookiecutter.project_slug}}/src/{{cookiecutter.project_slug}}/wsgi.py
@@ -6,4 +6,4 @@ from .app import app
 # manually enabled. By default debug mode is off. To enable dev mode, set the
 # environment variable `DASH_DEBUG` to `true`. You can also turn individual dev
 # tools features on using this method. See https://dash.plot.ly/devtools
-app.enable_dev_tools()
+app.enable_dev_tools(debug=False)
